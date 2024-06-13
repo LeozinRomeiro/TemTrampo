f6b0844ff81771e26cfe43d65d204e05f088116f
Leonardo Romeiro <leozinwork@gmail.com>
2024-06-13 20:01:12 -0300

    Fix: clean

--- Models/Vaga.cs
+++ Models/Vaga.cs
@@ -8,14 +8,14 @@ namespace TemTrampo.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
-        public string Requisito { get; set; }
-        public string Desejavel { get; set; }
-        public string Beneficios { get; set; }
-        public string Numero { get; set; }
-        public string Email { get; set; }
-        public string Escolaridade { get; set; }
-        public string Empresa { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public string? Requisito { get; set; }
+        public string? Desejavel { get; set; }
+        public string? Beneficios { get; set; }
+        public string? Numero { get; set; }
+        public string? Email { get; set; }
+        public string? Escolaridade { get; set; }
+        public string? Empresa { get; set; }
     }
 }
